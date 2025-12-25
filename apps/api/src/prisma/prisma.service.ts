5870133809c6f6bc8c4de467e28bcd4855d89cf1
ellanelee <ljedoc@naver.com>
2025-12-25 23:43:00 +0900

    cleanDatabase add

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -16,4 +16,26 @@ export class PrismaService
     await this.$disconnect();
     console.log('Data베이스 연결해지');
   }
+
+  async cleanDatabase() {
+    if (process.env.NODE_ENV === 'production') {
+      throw new Error('production실행시 cleanData를 실행불가');
+    }
+
+    const modelNames = Reflect.ownKeys(this).filter(
+      (key) =>
+        typeof key === 'string' &&
+        !key.startsWith('_') &&
+        key[0] === key[0].toLowerCase(),
+    ) as string[];
+
+    return await this.$transaction(async (tx) => {
+      for (const modelName of modelNames) {
+        const model = (tx as any)[modelName];
+        if (model && 'deleteMany' in model) {
+          await model.deleteMany();
+        }
+      }
+    });
+  }
 }
