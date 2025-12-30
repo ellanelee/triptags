2e0e286e82f55e980d3d7237def52b77ce1521bf
ellanelee <ljedoc@naver.com>
2025-12-30 23:53:38 +0900

    email로그인을 위한 guard와 strategy설정

---
+++ apps/api/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { PrismaService } from '@/prisma/prisma.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private configService: ConfigService,
+    private prisma: PrismaService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET')!,
+    });
+  }
+  async validate(payload: { sub: string }) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        id: payload.sub,
+      },
+    });
+    if (!user) {
+      throw new UnauthorizedException('사용자가 존재하지 않습니다');
+    }
+    return user;
+  }
+}
