c0a064143a56b12885139c022d35d6db2778f2e5
ellanelee <ljedoc@naver.com>
2026-01-03 23:59:56 +0900

    컨트롤러 _ 사용자 nickname변경 및 비밀번호 변경

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -1,9 +1,10 @@
 import {
+  Body,
   Controller,
   Get,
   HttpCode,
+  Param,
   Patch,
-  Put,
   UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
@@ -11,7 +12,13 @@ import { ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '@/auth/jwt-auth.guard.ts/jwt-auth.guard';
 import { CurrentUserId } from '@/common/decorator/current_user.decorator';
 import { JwtSubInfo } from '@/common/type/types';
-import { IUserResponse } from '@triptags/shared';
+import {
+  IUserNickname,
+  UpdatePasswordDto,
+  IUserPublicResponse,
+  IUserResponse,
+  IUserUpdate,
+} from '@triptags/shared';
 
 @ApiTags('users')
 @Controller('users')
@@ -21,25 +28,59 @@ export class UserController {
   @UseGuards(JwtAuthGuard)
   @Get('me')
   @HttpCode(204)
-  async userProfie(
+  async getUserProfie(
     @CurrentUserId() jwtUserInfo: JwtSubInfo,
   ): Promise<IUserResponse> {
-    return await this.userService.findById(jwtUserInfo.sub);
+    return await this.userService.findAllById(jwtUserInfo.sub);
   }
 
-  //Local User에만 있는 기본 정보 Update
+  //사용자 ID로 정보조회
+  @UseGuards(JwtAuthGuard)
+  @Get(':userId/profile')
+  @HttpCode(204)
+  async userPersonalInfo(
+    @Param('userId') targetUserId: string,
+    @CurrentUserId() jwtUserInfo: JwtSubInfo,
+  ): Promise<IUserPublicResponse> {
+    const loggedInUser = await this.getUserProfie(jwtUserInfo);
+    console.log('로그인된 유저정보 :', loggedInUser);
+    if (loggedInUser.role === 'ADMIN') {
+      return await this.userService.findAllById(targetUserId);
+    } else {
+      return await this.userService.findPubicInfoById(targetUserId);
+    }
+  }
+
+  //User 정보 Update (nickname, password제외)
   @UseGuards(JwtAuthGuard)
   @Patch('me')
   @HttpCode(204)
-  async updateLocalUserProfile(@CurrentUserId() jwtUserInfo: JwtSubInfo) {
-    return await this.userService.localUserProfileLoginUser(jwtUserInfo.sub);
+  async updateLocalUserProfile(
+    @CurrentUserId() jwtUserInfo: JwtSubInfo,
+    @Body() userUpdate: IUserUpdate,
+  ) {
+    return await this.userService.updateLoginUser(jwtUserInfo.sub, userUpdate);
   }
 
-  //전체 사용자의 세부 정보 Update
+  //nickname변경
   @UseGuards(JwtAuthGuard)
-  @Put('me')
+  @Get('changeNickname')
   @HttpCode(204)
-  async updateUserProfile(@CurrentUserId() jwtUserInfo: JwtSubInfo) {
-    return await this.userService.updateLoginUser(jwtUserInfo.sub);
+  async updateUserNickname(
+    @CurrentUserId() jwtUserInfo: JwtSubInfo,
+    @Body() userNickname: IUserNickname,
+  ) {
+    return this.userService.updateUserNickname(jwtUserInfo.sub, userNickname);
+  }
+
+  //로컬 사용자의 password변경
+  @UseGuards(JwtAuthGuard)
+  @Get('changeNickname')
+  @HttpCode(204)
+  async updateUserPassword(
+    @CurrentUserId() jwtUserInfo: JwtSubInfo,
+    @Body() passwordUpdate: UpdatePasswordDto,
+  ) {
+    return this.userService.updateUserPassword(jwtUserInfo.sub, passwordUpdate);
   }
 }
