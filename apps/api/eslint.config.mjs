65d942667b76353ea4312437314144a4abe5e1d1
ellanelee <ljedoc@naver.com>
2026-01-03 23:58:10 +0900

    공통 모듈에서 type import시 lint오류 수정_project의 tsconfig작업폴더 지정

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['**/eslint.config.*','**/dist/**', '**/node_modules/**'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
@@ -17,10 +17,11 @@ export default tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
+      sourceType: 'module',
       parserOptions: {
-        projectService: true,
         tsconfigRootDir: import.meta.dirname,
+        //모노레포의 타입오류 해결을 의해 tsconfig 직접 설정
+        project: ['./tsconfig.json', '../../packages/shared/tsconfig.json'],
       },
     },
   },
@@ -28,7 +29,7 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
 );
