6f4f6c3e80dc423a538e58cfd440e8bb74cbb092
ellanelee <ljedoc@naver.com>
2026-01-23 12:33:43 +0900

    :sparkles: feat:review조회기능구성

--- apps/api/src/review/review.controller.ts
+++ apps/api/src/review/review.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ReviewService } from './review.service';
+import { VenuePaginationDto } from '@triptags/shared';
 
 @Controller('reviews')
 export class ReviewController {
   constructor(private reviewService: ReviewService) {}
+
+  @Get()
+  async getReviewByVenueId(
+    @Param() venueId: string,
+    paginationDto: VenuePaginationDto,
+  ) {
+    return await this.reviewService.findReviewByVenueId(venueId, paginationDto);
+  }
 }
--- apps/api/src/review/review.service.ts
+++ apps/api/src/review/review.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@/prisma/prisma.service';
+import { VenuePaginationDto } from '@triptags/shared';
 
 @Injectable()
 export class ReviewService {
   constructor(private prisma: PrismaService) {}
 
-  async create() {}
+  async findReviewByVenueId(venueId: string, pageDto: VenuePaginationDto) {
+    const page = Number(pageDto.page) || 1;
+    const items = Number(pageDto.items) || 10;
+    const skip = (page - 1) * items;
+    const [totalCount, data] = await Promise.all([
+      this.prisma.client.review.count({ where: { venueId, deletedAt: null } }),
+      this.prisma.client.review.findMany({
+        where: { venueId, deletedAt: null },
+        skip,
+        take: items,
+        orderBy: { updatedAt: 'desc' },
+      }),
+    ]);
+    return {
+      items: data,
+      meta: {
+        totalCount,
+        page,
+        itemsPerPage: items,
+        totalPage: Math.ceil(totalCount / items),
+        hasNextPage: page < Math.ceil(totalCount / items),
+        hasPrevPage: page > 1,
+      },
+    };
+  }
 }
