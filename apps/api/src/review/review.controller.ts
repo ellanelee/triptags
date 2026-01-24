307603b4b6e7a1c62db93d76eebcbaab611cac66
ellanelee <ljedoc@naver.com>
2026-01-24 16:09:40 +0900

    :sparkles: feat: Review생성기능 구성

--- apps/api/src/review/review.controller.ts
+++ apps/api/src/review/review.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ReviewService } from './review.service';
-import { VenuePaginationDto } from '@triptags/shared';
+import { ReviewCreateDto, VenuePaginationDto } from '@triptags/shared';
 import { JwtAccessGuard } from '@/auth/jwt-auth.guard.ts/jwt-auth.access.guard';
+import { CurrentUser } from '@/common/decorator/current_user.decorator';
+import { User } from '@prisma/client';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+@ApiBearerAuth('access-token')
+@ApiTags('reviews')
 @Controller('reviews')
 export class ReviewController {
   constructor(private reviewService: ReviewService) {}
@@ -15,7 +20,17 @@ export class ReviewController {
     return await this.reviewService.findReviewByVenueId(venueId, paginationDto);
   }
 
-  @Post()
+  @Post(':venueId')
   @UseGuards(JwtAccessGuard)
-  async createReview() {}
+  async createReview(
+    @CurrentUser() user: User,
+    @Param('venueId') venueId: string,
+    @Body() reviewCreateDto: ReviewCreateDto,
+  ) {
+    return await this.reviewService.createReview(
+      venueId,
+      user.id,
+      reviewCreateDto,
+    );
+  }
 }
--- apps/api/src/review/review.service.ts
+++ apps/api/src/review/review.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '@/prisma/prisma.service';
-import { VenuePaginationDto } from '@triptags/shared';
+import { ReviewCreateDto, VenuePaginationDto } from '@triptags/shared';
 
 @Injectable()
 export class ReviewService {
@@ -30,5 +30,25 @@ export class ReviewService {
         hasPrevPage: page > 1,
       },
     };
+  }
+
+  async createReview(
+    venueId: string,
+    userId: string,
+    createDto: ReviewCreateDto,
+  ) {
+    const targetVenue = await this.prisma.client.venue.findFirst({
+      where: { id: venueId, deletedAt: null },
+    });
+    if (!targetVenue)
+      throw new NotFoundException('Review를 등록할 장소가 존재하지 않습니다');
+    await this.prisma.client.review.create({
+      data: {
+        rating: createDto.rating,
+        contents: createDto.contents,
+        venueId: venueId,
+        userId: userId,
+      },
+    });
   }
 }
