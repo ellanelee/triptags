a834017802c09d03bd94c88d284c0679ebccb6aa
ellanelee <ljedoc@naver.com>
2026-01-24 22:12:54 +0900

    :rotating_light: perf: 린트 경고수정

--- apps/api/src/review/review.controller.ts
+++ apps/api/src/review/review.controller.ts
@@ -15,7 +15,7 @@ import {
 } from '@triptags/shared';
 import { JwtAccessGuard } from '@/auth/jwt-auth.guard.ts/jwt-auth.access.guard';
 import { CurrentUser } from '@/common/decorator/current_user.decorator';
-import { Review, User } from '@prisma/client';
+import { User } from '@prisma/client';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @ApiBearerAuth('access-token')
