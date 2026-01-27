4dcb65a3c3ef322b621d2c483cb34712b1bf9936
ellanelee <ljedoc@naver.com>
2026-01-27 23:26:54 +0900

    :art: refactor: DTO표현 수정

--- packages/shared/src/dtos/tag/tagcreate.dto.ts
+++ packages/shared/src/dtos/tag/tagcreate.dto.ts
@@ -2,8 +2,8 @@ import { ApiProperty } from "@nestjs/swagger"
 
 export class TagCreateDto {
   @ApiProperty({
-    example: ["콩국수, 한식, 여의도 맛집"],
-    description: "장소에 관련된 tag",
+    example: ["콩국수", "한식", "여의도 맛집"],
+    description: "tag들을 각각 string으로 구분하여 배열로 입력",
   })
   tags!: string[]
 }
