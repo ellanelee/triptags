7e6dd3038d797532569c4dadbc4d84bde7a40a4c
ellanelee <ljedoc@naver.com>
2026-02-03 23:00:54 +0900

    :zap: perf: 국가코드 통일을 위해 library(i18n-iso-countries)반영

--- packages/shared/src/utils/location.ts
+++ packages/shared/src/utils/location.ts
@@ -16,17 +16,22 @@ countries.registerLocale(fr)
 countries.registerLocale(de)
 
 export const LocationUtils = {
-  // ISO 3166-1 alpha-2 적정성 검증
+  // ISO 3166-1 alpha-2를 이용한 국가 코드검증
   isValidCountryCode(countryCode: string): boolean {
     return countries.isValid(countryCode)
   },
 
-  //국가 코드의 국가명 변환
+  //국가명 변환
   getCountryName(countryCode: string, lang: string = "ko"): string {
-    return countries.getName(countryCode, lang) || countryCode
+    const countryName = countries.getName(countryCode, lang)
+    if (!countryName) {
+      console.error("해당하는 국가 이름이 없습니다")
+      throw new Error(`Invalid Country Code: ${countryCode}`)
+    }
+    return countryName
   },
 
-  //프론트엔드 국가 드룹다운 List
+  //프론트엔드 국가 List (드롭다운))
   selectCountryOption(lang: string = "ko") {
     const countryNames = countries.getNames(lang)
     return Object.entries(countryNames).map(([code, name]) => ({
