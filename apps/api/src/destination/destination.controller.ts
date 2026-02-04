576f9879c419dc303bf62b92736d89ab13c06986
ellanelee <ljedoc@naver.com>
2026-02-04 19:15:17 +0900

    :zap: perf: 선호 여행지 저장 구현

--- apps/api/src/destination/destination.controller.ts
+++ apps/api/src/destination/destination.controller.ts
@@ -1,10 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { DestinationService } from './destination.service';
+import { JwtAccessGuard } from '@/auth/jwt-auth.guard.ts/jwt-auth.access.guard';
+import { CurrentUser } from '@/common/decorator/current_user.decorator';
+import { User } from '@prisma/client';
+import { DestinationCreateDto } from '@triptags/shared';
 
 @ApiBearerAuth('access-token')
 @Controller()
 @ApiTags('destination')
 export class DestinationController {
   constructor(private destinationService: DestinationService) {}
+  @Post()
+  @UseGuards(JwtAccessGuard)
+  async handleCreateDestination(
+    @CurrentUser() user: User,
+    @Body() destinationDto: DestinationCreateDto,
+  ) {
+    return this.destinationService.createDestination(user.id, destinationDto);
+  }
 }
--- apps/api/src/destination/destination.service.ts
+++ apps/api/src/destination/destination.service.ts
@@ -1,7 +1,32 @@
 import { PrismaService } from '@/prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { DestinationCreateDto } from '@triptags/shared';
 
 @Injectable()
 export class DestinationService {
   constructor(private prisma: PrismaService) {}
+
+  //선호 여행지 등록
+  async createDestination(userId: string, createDto: DestinationCreateDto) {
+    const targetRegion = await this.prisma.client.region.findFirst({
+      where: {
+        name: createDto.district,
+        level: 3,
+        parent: {
+          name: createDto.city,
+          level: 2,
+        },
+      },
+      select: { id: true },
+    });
+    if (!targetRegion?.id)
+      throw new NotFoundException('등록할수 있는 region이 없습니다');
+    await this.prisma.client.destination.create({
+      data: {
+        userId: userId,
+        regionId: targetRegion.id,
+        priority: createDto.priority,
+      },
+    });
+  }
 }
