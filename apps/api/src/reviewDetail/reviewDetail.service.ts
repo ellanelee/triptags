3a923bc61938d4c999200285045ff80620b1298c
ellanelee <ljedoc@naver.com>
2026-02-09 12:37:48 +0900

    :zap: perf: reviewDetail로직 구현 및 swagger테스트후 수정

--- apps/api/src/reviewDetail/reviewDetail.service.ts
+++ apps/api/src/reviewDetail/reviewDetail.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@/prisma/prisma.service';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ReviewDetailCreateDto } from '@triptags/shared';
 
 @Injectable()
-export class ReviewDetailService {}
+export class ReviewDetailService {
+  constructor(private prisma: PrismaService) {}
+
+  async createReviewDetails(
+    reviewId: string,
+    createDto: ReviewDetailCreateDto,
+  ) {
+    const review = await this.prisma.client.review.findFirst({
+      where: { id: reviewId },
+    });
+    if (!review) throw new NotFoundException('Review를 찾을수 없습니다.');
+    const visitDate = createDto.visitDate
+      ? new Date(createDto.visitDate)
+      : undefined;
+    return await this.prisma.client.reviewDetail.upsert({
+      where: { reviewId: reviewId },
+      update: {
+        tasteRating: createDto.tasteRating,
+        serviceRating: createDto.ServiceRating,
+        priceRating: createDto.PriceRating,
+        visitDate: visitDate,
+        visitPurpose: createDto.visitPurpose,
+      },
+      create: {
+        tasteRating: createDto.tasteRating,
+        serviceRating: createDto.ServiceRating,
+        priceRating: createDto.PriceRating,
+        visitDate: visitDate,
+        visitPurpose: createDto.visitPurpose,
+        reviewId: reviewId,
+      },
+    });
+  }
+}
