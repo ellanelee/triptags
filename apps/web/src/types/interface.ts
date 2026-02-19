46a9f500448126ff1aad29041611be8aaad52659
ellanelee <ljedoc@naver.com>
2026-02-19 23:44:55 +0900

    :zap: perf: 언어변경 type설정 및 언어옵션 설정

--- apps/web/src/types/interface.ts
+++ apps/web/src/types/interface.ts
@@ -13,3 +13,10 @@ export interface IAuthResponse {
   accessToken: string
   user: IUserPublicResponse
 }
+
+export interface LanguageSelectProps {
+  label?: string
+  value: string
+  onChange: (value: string) => void
+  tr: (key: string) => string // 언어변역
+}
