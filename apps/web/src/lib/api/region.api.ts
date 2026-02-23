5ba709da44dfc3c72b3eaba2ce518fea8f6eb713
ellanelee <ljedoc@naver.com>
2026-02-23 23:21:29 +0900

    :zap: perf: 주소 변경구현을 위한 api설정변경

--- apps/web/src/lib/api/region.api.ts
+++ apps/web/src/lib/api/region.api.ts
@@ -4,11 +4,12 @@ import { count } from "console"
 
 export const regionApi = {
   create: async (data: RegionType) => {
-    const response = await apiClient.post("regions/region", data)
+    const response = await apiClient.post("regions", data)
   },
 
-  getRegionName: async (regionId: string) => {
-    const response = await apiClient.get(`regions/${regionId}`)
+  //지역명 가져오기
+  getRegionHierarchical: async (regionId: string) => {
+    const response = await apiClient.get(`regions/${regionId}/hierarchy`)
     if (!response.data.success) {
       throw new Error(
         response.data.message ?? response.data.error ?? "조회 실패",
@@ -18,9 +19,10 @@ export const regionApi = {
     return response.data.data ?? ""
   },
 
-  getCountryIdByCode: async (countryName: string) => {
-    const response = await apiClient.get(`regions/region`, {
-      params: { countryName },
+  //국가Code로 regionId가져오기
+  getCountryIdByCode: async (code: string) => {
+    const response = await apiClient.get(`regions/id`, {
+      params: { code },
     })
     if (!response.data.success) {
       throw new Error(
@@ -31,8 +33,9 @@ export const regionApi = {
     return response.data.data
   },
 
+  //regionId로 1단계 하단의 region가져오기
   getSubRegion: async (regionId: string) => {
-    const response = await apiClient.get("regions/regions", {
+    const response = await apiClient.get(`regions/${regionId}/sub`, {
       params: { regionId },
     })
     if (!response.data.success) {
--- apps/web/src/lib/api/user.api.ts
+++ apps/web/src/lib/api/user.api.ts
@@ -1,4 +1,4 @@
-import { LanguageDto } from "@triptags/shared"
+import { LanguageDto, UserAddressDto } from "@triptags/shared"
 import apiClient from "./api.client"
 
 export const userApi = {
@@ -12,10 +12,26 @@ export const userApi = {
     console.log(response.data)
     return response.data.data ?? ""
   },
-  
+
   updateLanguage: async (data: LanguageDto) => {
     const response = await apiClient.patch("users/language", data)
+    if (!response.data.success) {
+      throw new Error(
+        response.data.message ?? response.data.error ?? "조회 실패",
+      )
+    }
     console.log(response.data)
     return response.data
+  },
+
+  updateAddress: async (data: UserAddressDto) => {
+    const response = await apiClient.post("users/address", data)
+    if (!response.data.success) {
+      throw new Error(
+        response.data.message ?? response.data.error ?? "조회 실패",
+      )
+    }
+    console.log(response.data)
+    return response.data.data
   },
 }
