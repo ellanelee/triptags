099e3c6a59046aefdc1b8f7e9df5ed365cf1ca4f
ellanelee <ljedoc@naver.com>
2026-03-12 19:33:55 +0900

    :zap: perf: review생성 관련 type구성  및 오류 수정

---
+++ apps/web/src/types/interfaces/interface.form.ts
@@ -0,0 +1,6 @@
+import { I18nText } from "@triptags/shared"
+
+export interface ReviewForm {
+  rating: number
+  content: I18nText
+}
--- packages/shared/src/dtos/review/reviewcreate.dto.ts
+++ packages/shared/src/dtos/review/reviewcreate.dto.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger"
 import { IsInt, IsNotEmpty, Max, Min } from "class-validator"
 import { I18nText } from "../../common/types"
+import { UserRole } from "@triptags/database"
 
 export class ReviewCreateDto {
   @ApiProperty({
@@ -20,4 +21,11 @@ export class ReviewCreateDto {
   })
   @IsNotEmpty()
   contents!: I18nText
+
+  @ApiProperty({
+    example: ["USER", "USER_LOCAL", "BUSINESS", "ADMIN"],
+    description: "USER, USER_LOCAL , BUSINESS , ADMIN 중 택1",
+  })
+  @IsNotEmpty()
+  authorRole!: UserRole
 }
