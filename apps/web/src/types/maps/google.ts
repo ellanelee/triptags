8a28956e9f7bfde6a6e21f07df46a853fde77c1c
ellanelee <ljedoc@naver.com>
2026-03-27 23:39:22 +0900

    :zap: perf: 구글 정보 관련 type정의 및 message추가

---
+++ apps/web/src/types/maps/google.ts
@@ -0,0 +1,8 @@
+export interface IGooglePlaceInfo {
+  googlePlaceId: string
+  googleName: string
+  googleAddress: string
+  googleTypes: string[]
+  googleRating: number | undefined
+  googleUrl: string
+}
