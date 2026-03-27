e174afd9a41eb1dd0f88edba4765a17cf10c2935
ellanelee <ljedoc@naver.com>
2026-03-27 23:40:06 +0900

    :sparkles: feat: MapPick(마커)설정 컴퍼넌트

---
+++ apps/web/src/components/common/maps/MapPicket.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { MapPickerProps } from "@/types/maps/maps"
+import { AdvancedMarker, Map } from "@vis.gl/react-google-maps"
+import { useCallback, useState } from "react"
+
+const defaultCenter = { lat: 37.5665, lng: 126.978 } //Seoul, KR(중심위치)
+
+export default function MapPicker({
+  center = defaultCenter,
+  zoom = 13,
+  onLocationSelect,
+  markerPosition: externalMarkerPosition,
+  className = "",
+}: MapPickerProps) {
+  const [internalMarkerPosition, setInternalMarkerPosition] = useState<{
+    lat: number
+    lng: number
+  } | null>(null)
+
+  const markerPosition = externalMarkerPosition ?? internalMarkerPosition
+
+  const onMapClick = useCallback(
+    (e: any) => {
+      const latLng = e.detail?.latLng
+      if (latLng) {
+        const position = { lat: latLng.lat, lng: latLng.lng }
+        setInternalMarkerPosition(position)
+        onLocationSelect(position)
+      }
+    },
+    [onLocationSelect],
+  )
+
+  return (
+    <div>
+      <div className={`w-full h-100 rounded-lg overflow-hidden ${className}`}>
+        <Map
+          defaultCenter={center}
+          defaultZoom={zoom}
+          center={markerPosition || null}
+          zoom={zoom}
+          onClick={onMapClick}
+          gestureHandling="greedy"
+          disableDefaultUI={false}
+          clickableIcons={false}
+          mapId="venue_map_id"
+        />
+        {markerPosition && <AdvancedMarker position={markerPosition} />}
+      </div>
+    </div>
+  )
+}
